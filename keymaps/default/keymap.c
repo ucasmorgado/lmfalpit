3b6536cae4c6900c6849390df319d1f0ee5b54c9
lucas braga <112520005+ucasmorgado@users.noreply.github.com>
2025-03-11 18:43:42 -0300

    Add files via upload

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,25 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_5x12(
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L
+    )
+};
